62eeb92d5d41dd31e2934391b8138525f4f64eff
Paulo Araujo <paulofelipe_jau7654@hotmail.com>
2021-02-10 23:09:04 -0300

    create routes

--- src/routes/web.php
+++ src/routes/web.php
@@ -16,3 +16,6 @@
 $router->get('/', function () use ($router) {
     return $router->app->version();
 });
+
+$router->get('/city/{city}/artists', 'ArtistsByCityFactory@handle');
+$router->get('/latitude/{latitude}/longitude/{longitude}/artists', 'ArtistsByLatitudeLongitudeFactory@handle');
